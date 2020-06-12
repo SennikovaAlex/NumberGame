bccdc6dd87475c3987d05e36866ecc6c08254c0a
SennikovaAlex <66432547+SennikovaAlex@users.noreply.github.com>
2020-06-12 16:52:31 +0300

    Game final

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+  
 'use stirct'
 let isNumber = function(n) {
     return !isNaN(parseFloat(n)) && isFinite(n) 
@@ -7,35 +8,31 @@ function startGame () {
     let getRandomFloat = function (min, max) {
         return Math.round(Math.random() * (max - min) + min);
       };
-    
-
-    
     function numberUser() {
         let numberGame = getRandomFloat(1, 100);
         console.log(numberGame);
-        
-      let userNumber = prompt('Угадай число от 1 до 100'); 
+        let doNumber = function() {  
+            let userNumber = prompt('Угадай число от 1 до 100'); 
        function reaction() {
     if (userNumber == null) {
         alert('Game over'); 
     } else if (!isNumber(userNumber)) {
         alert ('Введите число');
-        numberUser() 
+        doNumber() 
     } else if (userNumber > numberGame) {
         alert ('Загаданное число меньше');
-     numberUser()
+        doNumber()
     } else if (userNumber < numberGame) {
         alert ('Загаданное число больше');
-        numberUser()
+        doNumber()
     } else if (userNumber == numberGame) {
         alert ('Поздравляю, вы победили')
         console.dir(reaction);
-    }
-       }
-       reaction(); 
-    }
+    } }
+    reaction();  } 
+    doNumber();
+    } 
     numberUser();
-   }
+}
     
     startGame();
-

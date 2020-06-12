7cd7fd3fe24ba1a739bf1c394977f477aaf5f853
Sennikova Alexandra <SennikovaAlexandra01@yandex.ru>
2020-06-12 14:12:15 +0300

    Легкая версия игры end

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 'use stirct'
 let isNumber = function(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n) //Если число конечное то тру
+    return !isNaN(parseFloat(n)) && isFinite(n) 
 };
 
 function startGame (number) {
-let numberGame = number;
+    let numberGame = number;
+    
+    function numberUser() {
+      let userNumber = prompt('Угадай число от 1 до 100'); 
+       function reaction() {
+    if (userNumber == null) {
+        alert('Game over'); 
+    } else if (!isNumber(userNumber)) {
+        alert ('Введите число');
+        numberUser() 
+    } else if (userNumber > numberGame) {
+        alert ('Загаданное число больше');
+     numberUser()
+    } else if (userNumber < numberGame) {
+        alert ('Загаданное число меньше');
+        numberUser()
+    } else if (userNumber == numberGame) {
+        alert ('Поздравляю, вы победили')
+        console.dir(reaction);
+    }
+       }
+       reaction(); 
+    }
+    numberUser();
+    
+    }
+    
+    startGame(56);
 
-function numberUser() {
-  let userNumber = prompt('Угадай число от 1 до 100'); 
-   function reaction() {
-if (userNumber == null) {
-    alert('Game over'); 
-} else if (!isNumber(userNumber)) {
-    alert ('Введите число');
-    numberUser() 
-} else if (userNumber > numberGame) {
-    alert ('Загаданное число больше');
- numberUser()
-} else if (userNumber < numberGame) {
-    alert ('Загаданное число меньше');
-    numberUser()
-} else if (userNumber == numberGame) {
-    alert ('Поздравляю, вы победили')
-}
-   }
-   reaction(); 
-}
-numberUser();
-}
 
-startGame(56);
 
 
 
